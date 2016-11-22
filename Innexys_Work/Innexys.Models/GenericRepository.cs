0eea7d879bdd926d32a872555c1d9b5356fb6740
bakasura980 <kariop980>
2016-11-22 07:28:02 +0300

    Add V.1

---
+++ Innexys_Work/Innexys.Models/GenericRepository.cs
@@ -0,0 +1,63 @@
+﻿using Innexys.Data;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Innexys.Models
+{
+    public class GenericRepository<T> : IRepository<T> where T : class
+    {
+
+        public GenericRepository()
+            :this(new DataContext()) { }
+
+        public GenericRepository(DataContext context)
+        {
+            this.DbContext = context;
+            this.DbSet = this.DbContext.Set<T>();
+        }
+
+        protected DbSet<T> DbSet { get; set; }
+
+        protected DbContext DbContext { get; set; }
+
+        public virtual IQueryable<T> All()
+        {
+            return this.DbSet.AsQueryable();
+        }
+
+        public virtual void Add(T entity)
+        {
+            DbEntityEntry entry = this.DbContext.Entry(entity);
+
+            if (entry.State != EntityState.Detached)
+            {
+                entry.State = EntityState.Detached;
+            }
+            else
+            {
+                this.DbSet.Add(entity);
+            }
+        }
+
+        public virtual void Update(T entity)
+        {
+            DbEntityEntry entry = this.DbContext.Entry(entity);
+            if (entry.State == EntityState.Detached)
+            {
+                this.DbSet.Attach(entity);
+            }
+            entry.State = EntityState.Modified;
+        }
+
+        public virtual int SaveChanges()
+        {
+            return this.DbContext.SaveChanges();
+        }
+    }
+}
---
+++ Innexys_Work/Innexys_Work/Security/CustomAuthorizeAttribute.cs
@@ -0,0 +1,30 @@
+﻿using Innexys_Work.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Innexys_Work.Security
+{
+    public class CustomAuthorizeAttribute : AuthorizeAttribute
+    {
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if (string.IsNullOrEmpty(SessionPersiter.Email))
+                filterContext.Result = new RedirectToRouteResult(
+                    new System.Web.Routing.RouteValueDictionary(new { controller = "Account", index = "Login" }));
+            else
+            {
+                AccountModel accmodel = new AccountModel();
+                CustomPrincipal customprincipal = new CustomPrincipal(accmodel.Find(SessionPersiter.Email));
+                if (!customprincipal.IsInRole(Roles))
+                {
+                    filterContext.Result = new RedirectToRouteResult(
+                        new System.Web.Routing.RouteValueDictionary(new { controller = "Error", action = "Login" }));
+                }
+            }
+                    
+        }
+    }
+}
