9cf406c44217ad9b074f827d12e6931c743103f3
bakasura980 <kariop980>
2017-05-09 09:20:13 +0300

    Add WebApiServer

---
+++ WebApi/WebApiTesting/Controllers/TasksController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiTesting.DataConnection;
+using WebApiTesting.Models;
+
+namespace WebApiTesting.Controllers
+{
+    public class TasksController : ApiController
+    {
+        IRepository<Task> tasks = new GenericRepository<Task>();
+        
+        // GET api/<controller>
+        public IEnumerable<Task> GetTasks()
+        {
+            return tasks.All();
+        }
+
+        // GET api/<controller>/5
+        public IHttpActionResult GetTask(int id)
+        {
+            Task searchingTask = tasks.GetExactly((task) => task.Id == id);
+
+            if (searchingTask != null)
+            {
+                return Ok(searchingTask);
+            }
+            return NotFound();
+        }
+
+        public IHttpActionResult GetLastTaskId()
+        {
+            if (tasks.All().Count > 0)
+            {
+                return Ok(tasks.All().Last().Id);
+            }else
+            {
+                return NotFound();
+            }
+        }
+
+        // POST api/<controller>
+        public void CreateTask([FromBody]Task newTask)
+        {
+            tasks.Add(newTask);
+            tasks.SaveChanges();
+        }
+
+        [HttpPut]
+        // PUT api/<controller>/5
+        public void UpdateTask([FromBody]Task editedTask)
+        {
+            tasks.Update(editedTask);
+            tasks.SaveChanges();
+        }
+    }
+}
---
+++ WebApi/WebApiTesting/Models/Account.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApiTesting.Models
+{
+    public class Account
+    {
+
+        private ICollection<Task> tasks;
+
+        public Account()
+        {
+            this.tasks = new HashSet<Task>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required]
+        public Position Position { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Format")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [Phone(ErrorMessage = "Invalid Phone Format")]
+        public string Phone { get; set; }
+
+
+        public virtual ICollection<Task> Tasks { get; set; }
+
+    }
+}
